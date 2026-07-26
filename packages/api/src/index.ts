0ef8b8eb5879f87303e1ad373f7aa338e0e195cf
beambeambeam <supawitmarayat@gmail.com>
2026-07-26 15:27:37 +0700

    chore: remove unused constants

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,4 @@
 export { createAppRouter } from "./router";
-export { awardOptions } from "./features/teams/teams.constants";
 export type { ApiDependencies, ApiRouter, AppRouter, AppRouterClient } from "./router";
 export type { ApiRole, ApiSession, ApiUser, AuthReader } from "./core/auth";
 export type { ApiContext } from "./core/context";

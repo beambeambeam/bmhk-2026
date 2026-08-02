ca4e7546075799e06f54f71874bc85a8fabda3a4
SupeemAFK <peemlomsak@gmail.com>
2026-08-02 22:22:27 +0700

    feat: add handle logout

--- apps/staff/src/routes/_auth/dashboard.tsx
+++ apps/staff/src/routes/_auth/dashboard.tsx
@@ -2,6 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { orpc } from "@bmhk-2026/client/orpc";
+import { Button } from "@base-ui/react/button";
+import { authClient } from "@bmhk-2026/client/auth-client";
+import { useNavigate } from '@tanstack/react-router'
 
 export const Route = createFileRoute("/_auth/dashboard")({
   component: RouteComponent,
@@ -9,14 +12,23 @@ export const Route = createFileRoute("/_auth/dashboard")({
 
 function RouteComponent() {
   const { session } = Route.useRouteContext();
+  const navigate = useNavigate();
 
   const privateData = useQuery(orpc.privateData.get.queryOptions());
+
+  function handleSignOut() {
+    authClient.signOut();
+    navigate({ to: "/login" });
+  } 
 
   return (
     <div>
       <h1>Dashboard</h1>
       <p>Welcome {session.data?.user.name}</p>
       <p>API: {privateData.data?.message}</p>
+      <Button className="p-2 bg-white rounded-md text-red-500 hover:bg-red-100 transition-all cursor-pointer" onClick={handleSignOut}>
+        Sign out test
+      </Button>
     </div>
   );
 }

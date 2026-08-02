ca8387fa535f7de491df012f1dde338aa4e1a6d7
SupeemAFK <peemlomsak@gmail.com>
2026-08-02 22:35:58 +0700

    fix: lint

--- apps/staff/src/routes/_auth/dashboard.tsx
+++ apps/staff/src/routes/_auth/dashboard.tsx
@@ -1,10 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 import { orpc } from "@bmhk-2026/client/orpc";
 import { Button } from "@base-ui/react/button";
 import { authClient } from "@bmhk-2026/client/auth-client";
-import { useNavigate } from '@tanstack/react-router'
 
 export const Route = createFileRoute("/_auth/dashboard")({
   component: RouteComponent,
@@ -16,17 +15,20 @@ function RouteComponent() {
 
   const privateData = useQuery(orpc.privateData.get.queryOptions());
 
-  function handleSignOut() {
-    authClient.signOut();
-    navigate({ to: "/login" });
-  } 
+  async function handleSignOut() {
+    await authClient.signOut();
+    await navigate({ to: "/login" });
+  }
 
   return (
     <div>
       <h1>Dashboard</h1>
       <p>Welcome {session.data?.user.name}</p>
       <p>API: {privateData.data?.message}</p>
-      <Button className="p-2 bg-white rounded-md text-red-500 hover:bg-red-100 transition-all cursor-pointer" onClick={handleSignOut}>
+      <Button
+        className="p-2 bg-white rounded-md text-red-500 hover:bg-red-100 transition-all cursor-pointer"
+        onClick={() => void handleSignOut()}
+      >
         Sign out test
       </Button>
     </div>

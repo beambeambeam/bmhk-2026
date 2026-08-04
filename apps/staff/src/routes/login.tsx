67a88df05f8c7e214752fdf2c6befd606368e9d8
SupeemAFK <peemlomsak@gmail.com>
2026-08-04 21:47:12 +0700

    feat: add redirect on already signin user login page to dashboard

--- apps/staff/src/routes/login.tsx
+++ apps/staff/src/routes/login.tsx
@@ -1,7 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import SignInForm from "@/features/auth/sign-in-form";
+import { authClient } from "@bmhk-2026/client/auth-client";
 
 export const Route = createFileRoute("/login")({
+  beforeLoad: async () => {
+    const session = await authClient.getSession();
+
+    if (session.data) {
+      throw redirect({
+        to: "/dashboard",
+      });
+    }
+  },
   component: SignInForm,
+  ssr: false,
 });

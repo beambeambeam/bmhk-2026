171ccf1c233fddce39171e15d3e5d86c56e7213d
Patipol Pongsawat <122760030+BooMzXI@users.noreply.github.com>
2026-08-18 04:11:30 +0700

    feat: port cookie consent banner and downloadable PDF document (#80)

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -4,6 +4,7 @@ import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { HeadContent, Outlet, Scripts, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import CookieConsent from "@/components/cookie-consent";
 
 import appCss from "../index.css?url";
 
@@ -50,6 +51,7 @@ function RootDocument() {
       </head>
       <body className="font-thai">
         <Outlet />
+        <CookieConsent />
         <Toaster richColors />
         <TanStackRouterDevtools position="bottom-left" />
         <ReactQueryDevtools position="bottom" buttonPosition="bottom-right" />
